42ffb3acc3cbf98f886eefedc44aa7cae7c97d0f
Robin KAWKA <rkawka@orange.fr>
2022-04-19 16:08:03 +0200

    Finition de la page d'accueil

--- script.js
+++ script.js
@@ -12,29 +12,51 @@ const imgRight = document.getElementById("img-right");
 const slidesTheme = document.querySelectorAll('.slide-theme');
 const themeText = document.querySelectorAll('.theme-text');
 const dots = document.querySelectorAll('.dot');
+const themesContainer = document.getElementById('themescontainer');
+const mainTitleTwo = document.getElementsByClassName("main-title")[1];
+const arrowDown = document.getElementsByClassName("arrow-down")[0];
+
+
+
+
+
+
+
 const backgroundData = [
   {
     theme: "Histoire",
+    alt: "Soldats en campagne.",
+    description:"Mesurez vos connaissances en histoire en repondant à des questions sur les guerres, les grandes récessions économiques ou encore sur les grandes figures historiques !",
     bgwelcome: 'https://images.unsplash.com/photo-1605811784653-ee4e230bd981?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
   },
   {
     theme: "Philosophie",
+    alt: "Bustes de différents philosophes.",
+    description:"Connaissez-vous vraiment la philosophie française, allemande ou encore celle de la Grèce antique sur le bout des doigts ?",
     bgwelcome: 'https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     theme: "Sciences",
+    alt: "Energie et éléctricité",
+    description:"Souvenez-vous de vos cours de lycées ! Physique, biologie, chimie, tout y passera !",
     bgwelcome: 'https://images.unsplash.com/photo-1507413245164-6160d8298b31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     theme: "Littérature",
+    alt: "Librairie contenant des centaiens de livres.",
+    description:"Flaubert, Proust, Dostoïevsky : savez-vous vraiment ce que ces génies de la littérature ont écrits ?",
     bgwelcome: 'https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1590&q=80',
   },
   {
     theme: "Sport",
+    alt: "Homme nageant le papillon.",
+    description:"Coupe du monde de football, JO, Rolland Garros: montrez-nous vos connaissances sportives !",
     bgwelcome: 'https://images.unsplash.com/photo-1530549387789-4c1017266635?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     theme:"Art",
+    alt: "Fresque murale representant une femme",
+    description:"\"L'art ne fait pas qu'illustrer, il fait vivre\" disait Nicolas ROBERT. Essayez-vous à notre quizz sur l'art !",
     bgwelcome: 'https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1245&q=80',
   }
   
@@ -62,6 +84,7 @@ var currentSlide = 0;
 changeBackground(currentSlide);
 showSlide(currentSlide);
 goSlide(currentSlide);
+showThemes();
 //Fonction au démarrage
 
 //Changement automatique des données
@@ -77,7 +100,7 @@ function autoChangeBackground() {
 
 
 
-
+arrowDown.addEventListener('click', bounceTitle)
 
 
 prevSlideTheme.addEventListener('click', ()=> {
@@ -139,6 +162,13 @@ function changeBackground(currentSlide) {
   imgRight.style.backgroundImage= ("url('"+bg_url+"')");
 }
 
+
+async function bounceTitle() {
+  await sleep(700);
+  mainTitleTwo.classList.add('animated');
+}
+
+
 async function changeOpacity() {
   var opacit = 0.3;
   for(let i = 0; i<50; i++) {
@@ -155,9 +185,26 @@ function sleep(ms) {
 
 
 
+function showThemes() {
+  themesContainer.innerHTML="";
+  for (let i = 0; i < backgroundData.length; i++) {
+    themeEl = document.createElement('div');
+    themeEl.classList.add('card');
+    themeEl.innerHTML = `
+    <div class="blur"></div>
+  <img src=${backgroundData[i].bgwelcome} alt="${backgroundData[i].alt}">
+  <div class="theme-info">
+      <h3 class="theme-text">${backgroundData[i].theme}</h3>
 
+  </div>
+  <div class="overview">
+      <h3 class="theme-text">${backgroundData[i].theme}</h3>
+      <p>${backgroundData[i].description}</p>
+  </div>`
+  themesContainer.appendChild(themeEl);
 
-
+  }
+}
 
 
 
